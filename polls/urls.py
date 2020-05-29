9e914a40e9e97586de4f0ee3fb492297bbd1fc86
hagios <hagioswilson@gmail.com>
2020-05-29 11:48:13 +0100

    used modelserializers

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from .views import polls_details, polls_list
 
 
 urlpatterns = [
 
-
+    path('polls', polls_list, name='poll_list'),
+    path('polls/<int:pk>', polls_details, name='polls_detail')
 
 ]
